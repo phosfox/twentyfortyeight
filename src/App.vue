1a09f75035667b772ccd60ab78bb04065b51fd9e
phosfox <mordredur@googlemail.com>
2021-03-16 01:16:30 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Board />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 
 @Component({
   components: {
-    HelloWorld
+    Board
   }
 })
 export default class App extends Vue {}
---
+++ src/components/Board.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <table>
+      <tr v-for="row in grid" :key="row">
+          <td v-for="cell in row" :key="cell">
+              {{cell}}
+          </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Board extends Vue {
+  get grid (): number[] {
+    return Array(4).fill(Array(4).fill(2))
+  }
+}
+</script>
+
+<style>
+</style>
